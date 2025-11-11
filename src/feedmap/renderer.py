4c7740cbf9d793ae734e402863b94c975878f129
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-11-11 20:19:42 +0100

    improved renderer and some template functions

--- src/feedmap/renderer.py
+++ src/feedmap/renderer.py
@@ -1,3 +1,4 @@
+import colorsys
 import geojson
 import logging
 import os
@@ -62,20 +63,31 @@ class JinjaRenderer:
 
     def _shade_filter(self, hex_color: str, level: int = 0) -> str:
         hex_color = hex_color.lstrip('#')
-        r, g, b = [int(hex_color[i:i + 2], 16) for i in (0, 2, 4)]
+        if len(hex_color) == 8:
+            r, g, b, a = [int(hex_color[i:i + 2], 16) / 255.0 for i in (0, 2, 4, 6)]
+        else:
+            r, g, b = [int(hex_color[i:i + 2], 16) / 255.0 for i in (0, 2, 4)]
+            a = 1.0
+
+        h, l, s = colorsys.rgb_to_hls(r, g, b)
         
         if level > 0:
-            factor: float = 1 + (level * 0.3)
+            l = min(1.0, l * (1 + level * 0.3))
+            s = max(0.0, s * 1.0)
         elif level < 0:
-            factor: float = 1 + (level * 0.1)
+            l = max(0.0, l * (1 + level * 0.1))
+            s = max(0.0, s * 0.9)
         else:
-            factor: float = 1.0
+            l = l
 
-        r = max(0, min(255, int(r * factor)))
-        g = max(0, min(255, int(g * factor)))
-        b = max(0, min(255, int(b * factor)))
+        r, g, b = colorsys.hls_to_rgb(h, l, s)
+        r, g, b = int(r*255), int(g*255), int(b*255)
+        a = int(a*255)
 
-        return f"#{r:02x}{g:02x}{b:02x}"
+        if a < 255:
+            return f"#{r:02x}{g:02x}{b:02x}{a:02x}"
+        else:
+            return f"#{r:02x}{g:02x}{b:02x}"
     
     def _text_filter(self, hex_color: str, level: int = 0) -> str:
         hex_color = self._shade_filter(hex_color, level)
@@ -89,9 +101,15 @@ class JinjaRenderer:
     def _geojson_filter(self, feature: Feature) -> str:
         return geojson.dumps(feature)
     
-    def _properties_filter(self, feature: Feature, propname: str = None) -> dict:
+    def _properties_filter(self, feature: dict|Feature, propname: str = None) -> dict:
         if propname is not None:
-            return feature.properties.get(propname, None)
+            if isinstance(feature, Feature):
+                return feature.properties.get(propname, None)
+            else:
+                return feature.get(propname, None)
         else:
-            return feature.properties
+            if isinstance(feature, Feature):
+                return feature.properties
+            else:
+                return feature
     
